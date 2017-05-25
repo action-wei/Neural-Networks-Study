3cf79f727770f59767c178be7e19c4efd8db117a
xiaowei3 <793089768@qq.com>
2017-05-25 14:40:03 +0800

    add Tensorflow_5_cnn_nlp

--- Tensorflow_4_cnn/cifar10_train.py
+++ Tensorflow_4_cnn/cifar10_train.py
@@ -47,7 +47,8 @@ FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train', """Directory where to write event logs """
                                                               """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 1000000, """Number of batches to run.""")
+# tutorials default set max_steps = 1000000, speeding up test, can set 100000
+tf.app.flags.DEFINE_integer('max_steps', 100000, """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False, """Whether to log device placement.""")
 tf.app.flags.DEFINE_integer('log_frequency', 10, """How often to log results to the console.""")
 
